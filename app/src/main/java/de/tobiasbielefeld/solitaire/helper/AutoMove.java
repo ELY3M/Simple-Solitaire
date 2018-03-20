07aabbbd7dc68c8ca6e8864e548aa84b30ac71c2
Tobias <tobias.bielefeld@gmail.com>
2018-03-20 13:22:27 +0100

    adding a "auto win" option for testing

--- app/src/main/java/de/tobiasbielefeld/solitaire/helper/AutoMove.java
+++ app/src/main/java/de/tobiasbielefeld/solitaire/helper/AutoMove.java
@@ -20,12 +20,6 @@ package de.tobiasbielefeld.solitaire.helper;
 
 import de.tobiasbielefeld.solitaire.handler.HandlerAutoMove;
 import de.tobiasbielefeld.solitaire.ui.GameManager;
-/**
- * if the last card on the tableau is flipped up, the auto complete can be run. it simply test
- * every card from the tableau and the stock if they can be placed on the foundation.
- * it continues until the last card was moved to the foundation. after that,
- * the win animation will be started
- */
 
 public class AutoMove {
 
